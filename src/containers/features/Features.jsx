310c265f92bd3d26d449440f4bd5ea269a9eb800
mikeymousey <mikelaplaine@gmail.com>
2022-03-23 22:03:56 -0400

    second coomit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './features.css';
+
+const Feature = () => {
+  return (
+    <div>Feature</div>
+  )
+}
+
+export default Feature
---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+const Features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Features
