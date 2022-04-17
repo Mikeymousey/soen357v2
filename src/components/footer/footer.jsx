af500b362e25b8121a4c89f833878afdb2601dfd
mikeymousey <mikelaplaine@gmail.com>
2022-04-17 19:08:35 -0400

    testing

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,38 +2,40 @@ import React from 'react';
 import medisafeLogo from '../../assets/medisafeLogo.png';
 import './footer.css';
 
-const Footer = () => (
-  <div className="medisafe__footer section__padding">
-    <div className="medisafe__footer-heading">
-      <h1 className="gradient__text">Unlock all the features</h1>
-    </div>
+const Footer = () => {
+    return (
+      <div className="medisafe__footer section__padding">
+        <div className="medisafe__footer-heading">
+          <h1 className="gradient__text">Unlock all the features</h1>
+        </div>
 
-    <div className="medisafe__footer-btn">
-      <p>Subscription</p>
-    </div>
+        <div className="medisafe__footer-btn">
+          <p>Subscription</p>
+        </div>
 
-    <div className="medisafe__footer-links">
-      <div className="medisafe__footer-links_logo">
-        <img src={medisafeLogo} alt="medisafe_logo" />
-        <p>Medisafe inc., <br /> All Rights Reserved</p>
-      </div>
-      <div className="medisafe__footer-links_div">
-        <h4>Links</h4>
-        <p>Social Media</p>
-        <p>Contact</p>
-      </div>
-      <div className="medisafe__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms and Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
-      </div>
-    </div>
+        <div className="medisafe__footer-links">
+          <div className="medisafe__footer-links_logo">
+            <img src={medisafeLogo} alt="medisafe_logo" />
+            <p>Medisafe inc., <br /> All Rights Reserved</p>
+          </div>
+          <div className="medisafe__footer-links_div">
+            <h4>Links</h4>
+            <p>Social Media</p>
+            <p>Contact</p>
+          </div>
+          <div className="medisafe__footer-links_div">
+            <h4>Company</h4>
+            <p>Terms and Conditions </p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+        </div>
 
-    <div className="medisafe__footer-copyright">
-      <p>@2022 Medisafe. All rights reserved.</p>
-    </div>
-  </div>
-);
+        <div className="medisafe__footer-copyright">
+          <p>@2022 Medisafe. All rights reserved.</p>
+        </div>
+      </div>
+    )
+  }
 
 export default Footer;
--- src/containers/index.js
+++
@@ -1,1 +0,0 @@
-export { default as WhatMS } from './whatMS/WhatMS';
