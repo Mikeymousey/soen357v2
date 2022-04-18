d8ff13289f2b4f1a0e58eb4896893f940ed82da4
mikeymousey <mikelaplaine@gmail.com>
2022-04-18 12:39:15 -0400

    added a calendar

--- src/components/notifications/Notifications.jsx
+++ src/components/notifications/Notifications.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './notifications.css';
 
 function Notifications() {
+    const [date, setDate] = useState(new Date());
     return (
         <div>
-            <h1>Notifications</h1>
+            <h1 className='medisafe__text-center'>Notifcation Calendar</h1>
+            <div className='medisafe__calendar-container'>
+                <Calendar onChange={setDate} value={date} />
+            </div>
+            <p className='medisafe__text-center'>
+                <span className='text-bold'>Selected Date:</span>{' '}
+                {date.toDateString()}
+            </p>
         </div>
     )
 }
